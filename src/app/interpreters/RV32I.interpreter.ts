1a85a9451c1380266ca793f8f905fa509dd4df75
Leaaaf <alessandro.foglia@outlook.it>
2020-01-31 12:36:59 +0100

    Modifiche interfaccia

--- src/app/interpreters/RV32I.interpreter.ts
+++ src/app/interpreters/RV32I.interpreter.ts
@@ -10,27 +10,8 @@ export class RV32Interpreter extends Interpreter {
         const B = "BEQ_BNE_BLT_BGE_BLTU_BGEU"
         const U = "LUI_AUIPC";
         const J = "JAL";
-
-        var typeInstruction = "";
-        var label = "";
-        var opcode = "";
-        var rd = "";
-        var rs1 = "";
-        var rs2 = "";
-        var immediate = "";
-        console.log("RV32I: " + line);
         
-        let myStr : string[] = line.split(';');
-        let args : string[] = myStr[0].split(' ');
-        for(let i = 0; i < args.length; i++) {
-            args[i] = args[i].replace(',', '');
-        }
-        if(args[0].includes(':')) {
-            label = args[0];
-            opcode = args[1];
-        } else {
-            opcode = args[0];
-        }
+        console.log("RV32I: " + line);
         
     }
 }
