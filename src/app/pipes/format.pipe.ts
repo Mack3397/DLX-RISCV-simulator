daf961750686a05df1190cddbdedd24e085d0752
Macca <fabrik97@live.com>
2020-01-31 12:27:14 +0100

    modifica pipe format

--- src/app/pipes/format.pipe.ts
+++ src/app/pipes/format.pipe.ts
@@ -5,14 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatPipe implements PipeTransform {
 
-  transform(n: number, type: 'dec'|'bin'|'hex'): any {
+  transform(n: number, type: 'dec'|'bin'|'hex', length: number = 32): any {
     switch (type) {
       case 'dec':
         return n
       case 'bin':
-        return (n >>> 0).toString(2).padStart(32,'0');
+        return (n >>> 0).toString(2).padStart(length,'0');
       case 'hex':
-        return '0x'+(n >>> 0).toString(16).padStart(8,'0').toUpperCase();
+        return '0x'+(n >>> 0).toString(16).padStart(Math.ceil(length / 4),'0').toUpperCase();
     }
     return null;
   }
