bca5370a4b3b1f628b9584404fa34c9cceb937f6
Leaaaf <alessandro.foglia@outlook.it>
2020-02-11 17:55:05 +0100

    Fixed memory #6

--- src/app/memory/memory.component.ts
+++ src/app/memory/memory.component.ts
@@ -30,11 +30,18 @@ export class MemoryComponent implements OnInit {
   onAdd() {
     let firstFreeAddr = this.memoryService.memory.firstFreeAddr;
     this.memoryService.add('New', firstFreeAddr, firstFreeAddr + 0x01FFFFFF)
+    this.memoryService.save();
   }
 
   onDelete(dev: Device) {
     this.memoryService.remove(dev);
     this.selected = null;
+    this.memoryService.save();
+  }
+
+  onChange(event:any) {
+    // ((this.memoryService.memory.devices.find((dev) => dev.checkAddress(this.selected.max_address)) != this.selected) || (this.memoryService.memory.devices.find((dev) => dev.checkAddress(this.selected.min_address)) != this.selected)) ? window.alert("Error") : (parseInt(this.selected.size) >= 128 ? this.memoryService.save() : window.alert("Error"));
+    this.memoryService.memory.devices.some((dev) => ((this.selected.checkAddress(dev.min_address) || this.selected.checkAddress(dev.max_address)) && dev.name != this.selected.name)) ? window.alert("Error") : (parseInt(this.selected.size) >= 128 ? this.memoryService.save() : window.alert("Memory is less than 128 MB"));
   }
 
 }
